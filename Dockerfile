9199abb56621cef636437dce5b8d79dfc0b620b0
Priyansh Jain <priyansh.jain0246@gmail.com>
2019-03-15 01:13:03 +0530

    added some frontend + download

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,11 @@ WORKDIR /usr/src/app
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
-# COPY package*.json ./
+COPY package*.json ./
 
-# RUN npm install
+RUN npm install
 # If you are building your code for production
 # RUN npm install --only=production
 
 # Bundle app source
-# COPY . .
+COPY . .
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,30 @@
 var express = require("express");
 var router = express.Router();
 var multer = require("multer");
-const request = require("request-promise");
+const request = require("request-promise").defaults({ encoding: null });
 var upload = multer({ dest: "uploads/" });
 const fs = require("fs");
 const path = require("path");
+const sha1 = require("js-sha1");
 
+const mapPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "data",
+  "maps-" + process.env.SELF_HOSTNAME.split(".example.com")[0] + ".json"
+);
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", {
+    title: "Storage Virtualization",
+    success: true,
+    message: ""
+  });
 });
 
 router.post("/upload", upload.single("uploadFile"), async (req, res, next) => {
-  let detailsArray = [
-    {
-      name: req.file.originalname,
-      path: req.file.path,
-      mimetype: req.file.mimetype,
-      hostname: process.env.SELF_HOSTNAME
-    }
-  ];
+  let detailsArray = [];
   // get all servers
   // static for now
   let hostnames = [
@@ -29,8 +34,6 @@ router.post("/upload", upload.single("uploadFile"), async (req, res, next) => {
     "server4.example.com",
     "server5.example.com"
   ];
-  const selfName = process.env.SELF_HOSTNAME;
-  hostnames = hostnames.filter(n => n !== selfName);
   let hostnamesToBackup = [];
   hostnamesToBackup[0] =
     hostnames[Math.floor(Math.random() * hostnames.length)];
@@ -46,6 +49,22 @@ router.post("/upload", upload.single("uploadFile"), async (req, res, next) => {
   }
   const backupUrl = "/backup";
   const mapUrl = "/updateMaps";
+  let fileContent = fs.readFileSync(req.file.path);
+  let fileStream = fs.createReadStream(req.file.path);
+  const fileHash = sha1(fileContent);
+  let map;
+  let mapJson = {};
+  if (fs.existsSync(mapPath)) {
+    map = fs.readFileSync(mapPath);
+    mapJson = JSON.parse(map);
+  }
+  if (mapJson[fileHash]) {
+    return res.render("index", {
+      message: "File already exists",
+      success: false,
+      title: "Storage Virtualization"
+    });
+  }
 
   let promises = [];
   for (const hostname of hostnamesToBackup) {
@@ -57,7 +76,7 @@ router.post("/upload", upload.single("uploadFile"), async (req, res, next) => {
       },
       formData: {
         backupFile: {
-          value: fs.createReadStream(req.file.path),
+          value: fileStream,
           options: {
             filename: req.file.originalname,
             contentType: req.file.mimetype
@@ -67,38 +86,28 @@ router.post("/upload", upload.single("uploadFile"), async (req, res, next) => {
     };
     promises.push(request(backupOptions));
   }
+  fs.unlinkSync(req.file.path);
   let results = await Promise.all(promises);
 
   results.forEach(result => {
     detailsArray.push(JSON.parse(result).details);
   });
 
-  const mapPath = path.join(
-    __dirname,
-    "..",
-    "..",
-    "data",
-    "maps-" + process.env.SELF_HOSTNAME.split(".example.com")[0] + ".json"
-  );
-  let map;
-  let mapJson = {};
-  if (fs.existsSync(mapPath)) {
-    map = fs.readFileSync(mapPath);
-    mapJson = JSON.parse(map);
-  }
+  let backups = [];
   detailsArray.forEach(detail => {
-    const { hostname, ...others } = detail;
-    if (mapJson[detail.hostname]) {
-      mapJson[detail.hostname].push(others);
-    } else {
-      mapJson[detail.hostname] = [others];
-    }
+    backups.push({
+      hostname: detail.hostname,
+      path: detail.path
+    });
   });
-
+  const { hostname, path, ...others } = detailsArray[0];
+  mapJson[fileHash] = {
+    details: { ...others },
+    backups
+  };
   fs.writeFileSync(mapPath, JSON.stringify(mapJson));
 
   let mapPromises = [];
-  console.log(hostnames);
   for (const hostName of hostnames) {
     var options = {
       method: "POST",
@@ -109,7 +118,11 @@ router.post("/upload", upload.single("uploadFile"), async (req, res, next) => {
     mapPromises.push(request(options));
   }
   results = await Promise.all(promises);
-  res.json({ success: true });
+  res.render("index", {
+    title: "Storage Virtualization",
+    success: true,
+    message: "Successfully uploaded!"
+  });
 });
 
 router.post("/backup", upload.single("backupFile"), (req, res, next) => {
@@ -117,7 +130,9 @@ router.post("/backup", upload.single("backupFile"), (req, res, next) => {
     name: req.file.originalname,
     path: req.file.path,
     mimetype: req.file.mimetype,
-    hostname: process.env.SELF_HOSTNAME
+    hostname: process.env.SELF_HOSTNAME,
+    date: new Date().toUTCString(),
+    size: req.file.size
   };
   res.json({ success: true, details });
 });
@@ -135,4 +150,85 @@ router.post("/updateMaps", (req, res, next) => {
   );
   return res.json({ success: true });
 });
+
+router.get("/fileList", (req, res, next) => {
+  if (!fs.existsSync(mapPath)) {
+    return res.render("list", { message: "No files exist.", map: [] });
+  }
+  let map = JSON.parse(fs.readFileSync(mapPath));
+
+  map = Object.keys(map).map(elem => {
+    const details = map[elem].details;
+    return {
+      name: details.name,
+      size: details.size,
+      hash: elem,
+      date: details.date
+    };
+  });
+
+  res.render("list.ejs", { map, message: "" });
+});
+
+router.get("/download", async (req, res, next) => {
+  const fileHash = req.query.hash;
+  if (!fs.existsSync(mapPath)) {
+    return res.json({ message: "no files exist" });
+  }
+  let map = JSON.parse(fs.readFileSync(mapPath));
+  const fileDetails = map[fileHash].details;
+  let fileBackups = map[fileHash].backups;
+  let possiblePresentFile = fileBackups.filter(
+    file => file.hostname === process.env.SELF_HOSTNAME
+  );
+
+  if (possiblePresentFile.length !== 0) {
+    return res.download(possiblePresentFile[0].path, fileDetails.name);
+  }
+  // take a random path
+  let extPathIndex = Math.floor(Math.random() * fileBackups.length);
+  console.log(extPathIndex);
+
+  const fileHostDetails = fileBackups[extPathIndex];
+
+  try {
+    request.get(
+      "http://" +
+        fileHostDetails.hostname +
+        ":3000/downloadFromPath?path=" +
+        fileHostDetails.path,
+      (err, response, body) => {
+        const tempPath = path.join(
+          __dirname,
+          "..",
+          "..",
+          "data",
+          fileDetails.name
+        );
+
+        fs.writeFile(tempPath, body, err => {
+          if (err) {
+            console.log("error");
+            throw err;
+          }
+
+          res.download(tempPath, fileDetails.name, () => {
+            fs.unlinkSync(tempPath);
+          });
+        });
+      }
+    );
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.get("/downloadFromPath", (req, res, next) => {
+  try {
+    return res.sendFile(path.join(__dirname, "..", "..", req.query.path));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
