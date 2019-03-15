f72d68f7e46670f5778c97dd889c0a9d99e04710
Priyansh Jain <priyansh.jain0246@gmail.com>
2019-03-15 10:35:04 +0530

    final build

--- create-network.sh
+++ create-network.sh
@@ -9,4 +9,5 @@ docker swarm leave -f
 docker swarm init
 docker network create --attachable --driver overlay --subnet=10.200.1.0/24 "$NETWORK_NAME"
 
-docker stack deploy -c docker-compose.yaml test
+
+# docker stack deploy -c docker-compose.yaml test
