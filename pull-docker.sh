fddedf7dc319d1b0a6643746824dc60855046653
Priyansh Jain <priyansh.jain0246@gmail.com>
2019-04-08 04:42:22 +0530

    port change in prod

--- pull-docker.sh
+++ pull-docker.sh
@@ -3,5 +3,4 @@ mkdir -p /tmp/uploads
 rm -rf /tmp/uploads/*
 docker pull presto412/storev1:latest
 docker swarm leave -f
-docker swarm join --token SWMTKN-1-13bdl2qdigqernaevy9o86mt1kbwtvdrrxjbm4ev6dsyuffw7t-6utpdwjjjtxt12s26rnomznjz 178.128.147.66:2377
-
+docker swarm join --token SWMTKN-1-3tjsxp8ix2vcck4com1h47lxt7qpb60k7pcguhi6yv4iqarnwl-eiladdltl3di0h98fjg5xya5k 178.128.147.66:2377
