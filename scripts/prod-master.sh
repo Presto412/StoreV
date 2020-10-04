fec142d5c559b9faca94d073c2d2b340de98f525
Priyansh Jain <priyansh.jain0246@gmail.com>
2020-10-04 13:37:04 +0530

    Update prod-master.sh

--- scripts/prod-master.sh
+++ scripts/prod-master.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-# set -ev
 
 ENV_LOCATION=$PWD/.env
 source $ENV_LOCATION
+USER="root"
 
-ssh root@"$STORAGE_TORONTO_IP" 'bash -s' < ./scripts/install-docker.sh
-ssh root@"$STORAGE_AMSTERDAM_IP" 'bash -s' < ./scripts/install-docker.sh
-ssh root@"$STORAGE_BANGALORE_IP" 'bash -s' < ./scripts/install-docker.sh
-ssh root@"$STORAGE_SINGAPORE_IP" 'bash -s' < ./scripts/install-docker.sh
-ssh root@"$CENTRAL_NYC_IP" 'bash -s' < ./scripts/install-docker.sh
+ssh "$USER"@"$STORAGE_TORONTO_IP" 'bash -s' < ./scripts/install-docker.sh
+ssh "$USER"@"$STORAGE_AMSTERDAM_IP" 'bash -s' < ./scripts/install-docker.sh
+ssh "$USER"@"$STORAGE_BANGALORE_IP" 'bash -s' < ./scripts/install-docker.sh
+ssh "$USER"@"$STORAGE_SINGAPORE_IP" 'bash -s' < ./scripts/install-docker.sh
+ssh "$USER"@"$CENTRAL_NYC_IP" 'bash -s' < ./scripts/install-docker.sh
 
 
-ssh root@"$STORAGE_TORONTO_IP" 'bash -s' < ./scripts/pull-docker.sh
-ssh root@"$STORAGE_AMSTERDAM_IP" 'bash -s' < ./scripts/pull-docker.sh
-ssh root@"$STORAGE_BANGALORE_IP" 'bash -s' < ./scripts/pull-docker.sh
-ssh root@"$STORAGE_SINGAPORE_IP" 'bash -s' < ./scripts/pull-docker.sh
-ssh root@"$CENTRAL_NYC_IP" 'bash -s' < ./scripts/pull-docker.sh
+ssh "$USER"@"$STORAGE_TORONTO_IP" 'bash -s' < ./scripts/pull-docker.sh
+ssh "$USER"@"$STORAGE_AMSTERDAM_IP" 'bash -s' < ./scripts/pull-docker.sh
+ssh "$USER"@"$STORAGE_BANGALORE_IP" 'bash -s' < ./scripts/pull-docker.sh
+ssh "$USER"@"$STORAGE_SINGAPORE_IP" 'bash -s' < ./scripts/pull-docker.sh
+ssh "$USER"@"$CENTRAL_NYC_IP" 'bash -s' < ./scripts/pull-docker.sh
 
-CMD="$(ssh root@$CENTRAL_NYC_IP 'bash -s' < create-network.sh | grep -P 'docker swarm join --token SWMTKN.*' -o)"
+scp .env "$USER@$CENTRAL_NYC_IP:.env"
+scp "./docker-compose-$NODE_ENV.yaml" "$USER@$CENTRAL_NYC_IP:docker-compose-$NODE_ENV.yaml"
 
-ssh root@"$STORAGE_TORONTO_IP" $CMD
-ssh root@"$STORAGE_AMSTERDAM_IP" $CMD
-ssh root@"$STORAGE_BANGALORE_IP" $CMD
-ssh root@"$STORAGE_SINGAPORE_IP" $CMD
+CMD=$(ssh "$USER"@"$CENTRAL_NYC_IP" < ./scripts/create-network.sh | grep 'docker swarm join --token SWMTKN.*' -o)
 
-ssh root@"$CENTRAL_NYC_IP" "bash -s docker stack deploy -c docker-compose-"$NODE_ENV".yaml test"
+ssh "$USER"@"$STORAGE_TORONTO_IP" "$CMD"
+ssh "$USER"@"$STORAGE_AMSTERDAM_IP" "$CMD"
+ssh "$USER"@"$STORAGE_BANGALORE_IP" "$CMD"
+ssh "$USER"@"$STORAGE_SINGAPORE_IP" "$CMD"
+
+ssh "$USER"@"$CENTRAL_NYC_IP" "docker stack deploy -c docker-compose-$NODE_ENV.yaml test"
